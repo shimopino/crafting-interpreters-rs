8289892faf4094ad321dc4b2f4f09702f0f37697
shimopino <shimoke4869@gmail.com>
2023-11-04 15:40:53 +0000

    feat: Parserの追加

---
+++ src/parser.rs
@@ -0,0 +1,12 @@
+use crate::token::Token;
+
+struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, current: 0 }
+    }
+}
